764c7cbd4f57b89a99560cf6bd663523dad7b48c
Ramapr <arcabuceroma79@gmail.com>
2024-04-15 21:41:39 +0300

    fixrun.sh

--- 1-base_service/run.sh
+++ 1-base_service/run.sh
@@ -3,7 +3,7 @@
 base_port = 5000
 
 for i in {1..2}
-    port = $base_port+$1
+    port = $base_port+$i
     echo port
     do docker run —-name "server-r${i}" -d -e REPLICA_NAME="replica-${i}" -e PORT=port -p port:5000 server:new
     sleep 3
