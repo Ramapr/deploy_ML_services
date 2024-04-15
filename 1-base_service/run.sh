d203dd8d4acca9dbd2243193c693980cc6b7b396
Ramapr <arcabuceroma79@gmail.com>
2024-04-15 22:35:40 +0300

    Update run.sh

--- 1-base_service/run.sh
+++ 1-base_service/run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
 base_port=5000
 
-for i in {1..2}; do
-    port=$base_port+$i
-    docker run —-name "server-${i}" -d -e REPLICA_NAME="replica-${i}" -e PORT=port -p port:5000 server:new
-    sleep 3    
+for i in {1..3}; do
+    port=`expr $base_port + $i`
+    echo $port
+    docker run --name "server-${i}" -d --env REPLICA_NAME="replica-${i}" --env PORT=$port -p $port:5000 server:new
+    sleep 3
 done
